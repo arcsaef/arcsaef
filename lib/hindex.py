d180dea0d3fc9f515ed72408dfc42c273ce17934
EpicSalsa <EpicSalsa@users.noreply.github.com>
2024-09-02 20:10:48 +1000

    hindex lib abd front end files

--- lib/hindex.py
+++ lib/hindex.py
@@ -1,63 +1,78 @@
 import requests
-import xml.etree.ElementTree as ET # cos I can't get json working
 import time
 import sys
 import yaml
+import json
 
+''' Get hold of the openalex api and orcid, full name doubles '''
 def get_data(config_file='config/reporting.yaml'):
-	result      = {}
+	hdrs     	  = {}
 	author_list = []
 
 	with open(config_file, 'r') as file:
 		cf = yaml.safe_load(file)
 
-	with open(cf['data']['saef_scopus']) as author_file:
+	with open(cf['data']['orcid']) as author_file:
 		for author in author_file:
 			author_list.append(author.strip())
 
-	result['url']    = cf['openalex']['url']
-	result['auth']   = author_list
+	hdrs['url']    = cf['openalex']['url']
+	hdrs['orcids'] = author_list
 
-	return result
+	return hdrs
 
+''' Get author page '''
+def author_page(url):
 
-def author_retrieval(orcid):
-	time.sleep(1) # sleep for 1 second
-	
-	hdrs = get_data()
-
-	api_url  = f"{hdrs.get('url')}{orcid}"
-	response = requests.get(api_url) 
+	response = requests.get(url) 
   
 	if response.status_code == 200:
-		return response.content.decode('utf-8')
+		return json.loads(response.content) # dictionary 
 	else:
-		return response
+		return response.status_code 
 
-def h_index(page, authorid):
-  # page = author_retrieval(author)
+''' Get an author h-index score'''
+def hindex(author_page):
 
-  if type(page) != 'int':          # No error codes allowed
-    tree = ET.fromstring(page)
-    # tree[1].text # contains h-index score
+	# No error codes allowed
+  if isinstance(author_page, dict):          
+    return int(author_page.get('summary_stats')['h_index'])
+  else:
+  	return None
 
-  with open('scopus_hindex_2024.txt', 'a') as file:
-  	file.write(f"{authorid}, {tree[1].text}\n")
+''' Get the number of articles with >= 2.5 impact factor '''
+def impact_factor(author_page):
+	count = 0
 
-# define h_index KPI
-def hindex_distribution(filename):
+	# No error codes allowed
+	if isinstance(author_page, dict):
+		return int(author_page.get('summary_stats')['2yr_mean_citedness'])
+	else:
+ 		return None
+
+''' Define h-index KPI '''
+def hindex_distribution(hindexes):
   gt80, gt70, gt50, gt40, gt20 = 0, 0, 0, 0, 0
-  with open(filename, 'r') as file:
-    for line in file:
-      score = int(line.split(',')[1])
-      if score > 80:
-        gt80 += 1
-      if score > 70:
-        gt70 += 1
-      if score > 50:
-        gt50 += 1
-      if score > 40:
-        gt40 += 1
-      if score > 20:
-        gt20 += 1
+
+  for hindex in hindexes:
+    if hindex >= 80:
+      gt80 += 1
+    if hindex >= 70 and hindex < 80:
+      gt70 += 1
+    if hindex >= 50 and hindex < 70:
+      gt50 += 1
+      pass
+    if hindex >= 40 and hindex < 50:
+      gt40 += 1
+      pass
+    if hindex >= 20 and hindex < 40:
+      gt20 += 1
   return f">80: {gt80} >70: {gt70} >50: {gt50} >40: {gt40} >20: {gt20}"
+
+''' Define >2.5 impact factor qty '''
+def impact_factor_gt25(impact_factors):
+	count = 0
+	for i in impact_factors:
+		if i >= 2.5:
+			count += 1
+	return count
