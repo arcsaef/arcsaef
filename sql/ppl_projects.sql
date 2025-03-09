126b67e73ccbbced720b4ec26dbb04c375e37bb0
EpicSalsa <EpicSalsa@users.noreply.github.com>
2025-03-09 14:29:31 +1000

    sql based reporting

---
+++ sql/ppl_projects.sql
@@ -0,0 +1,8 @@
+--  For each name, return a project(s) string
+SELECT REPLACE(a.fullname,' ', '') 	AS Prsn, 
+	   GROUP_CONCAT(b.project ) 	AS Projects
+FROM ppl_projects a
+JOIN projectKey b ON a.id_project 	= b.id_project
+WHERE a.person_status  				= 'Active'
+AND   a.project_status 				= 'Active'
+GROUP BY a.fullname;
