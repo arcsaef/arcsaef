a5510ded7a9fb6892f21e8469ba91163ffb69742
EpicSalsa <EpicSalsa@users.noreply.github.com>
2025-04-16 12:22:07 +1000

    year update

--- sql/crosstab_acp.sql
+++ sql/crosstab_acp.sql
@@ -14,23 +14,23 @@ JOIN tmp_outputs_author b
 	ON a.author_key= b.author_key
 WHERE a.position in ('Associate Investigator', 'Chief Investigator',
  'Partner Investigator')
-AND b.pub_yr = 2023
+AND b.pub_yr = 2024
 UNION
 SELECT 
 a.position
   , a.author_key 
-	, "2023" as pub_yr
+	, "2024" as pub_yr
 	, "" AS title 
 FROM ppl a
 WHERE a.author_key NOT IN ( SELECT a.author_key
 							FROM ppl a
 							JOIN tmp_outputs_author b 
 								ON a.author_key= b.author_key
-							WHERE b.pub_yr = 2023
+							WHERE b.pub_yr = 2024
 							AND a.position in ('Associate Investigator', 
 								'Chief Investigator', 'Partner Investigator'))
 AND a.position in ('Associate Investigator', 'Chief Investigator',
  'Partner Investigator')
-AND a.start_dt <= '2023-12-31' -- Commenced b/4 the end of calendar year
-AND a.end_dt > '2022-12-31' OR a.end_dt is NULL -- Terminated after start of calendar year or on going
+AND a.start_dt <= '2024-12-31' -- Commenced b/4 the end of calendar year
+AND a.end_dt > '2023-12-31' OR a.end_dt is NULL -- Terminated after start of calendar year or on going
 ORDER BY 1, 2
