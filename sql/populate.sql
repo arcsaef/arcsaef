bb562a9fe6df3b69ebe9fdd685f9ddd9cee6b57e
EpicSalsa <EpicSalsa@users.noreply.github.com>
2025-07-10 15:10:49 +1000

    Update populate.sql
    added new fields, updated renamed columns

--- sql/populate.sql
+++ sql/populate.sql
@@ -1,14 +1,19 @@
 --
-INSERT INTO ppl_projects (id_person, id_project, project_status, fullname, person_status)
+INSERT INTO ppl_projects (id_person, id_project, project_code
+                          , project_status, fullname, person_status
+                          , project_role, project_fte)
 SELECT 
   ppl.id_person, 
-  tmp_ppl_projects.id_project,
+  tmp_ppl_projects.idf_project,
+  projects.project_code,
   projects.project_status,
   ppl.first_name || " " || ppl.last_name,
-  ppl.status
+  ppl.status,
+  tmp_ppl_projects.role,
+  tmp_ppl_projects.fte
 FROM  ppl, tmp_ppl_projects, projects
-WHERE ppl.id_person = tmp_ppl_projects.id_person
-AND   tmp_ppl_projects.id_project = projects.id_project;
+WHERE ppl.id_person = tmp_ppl_projects.idf_person
+AND   tmp_ppl_projects.idf_project = projects.id_project;
 
 -- Create author key
 UPDATE ppl SET author_key = first_name || last_name;
